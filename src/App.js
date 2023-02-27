72bc1479efe38e29428b2a1a26bcaad317fb015a
TauqeerAhmad5201 <hellotauqeer@gmail.com>
2023-02-27 23:53:38 +0530

    added Event in App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Navbar from './components/Navbar'
+import Event from './components/Event'
 
 function App() {
   return (
     <>
     <Navbar></Navbar>
+    <Event></Event>
     </>
   );
 }
