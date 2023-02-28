06a947418bd744d87a557234446f28e6f0a44b3f
TauqeerAhmad5201 <hellotauqeer@gmail.com>
2023-02-28 23:38:38 +0530

    added detail tab in App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Navbar from './components/Navbar'
 import Event from './components/Event'
+import Details from './components/Details';
 
 function App() {
   return (
     <>
     <Navbar></Navbar>
     <Event></Event>
+    <Details></Details>
     </>
   );
 }
