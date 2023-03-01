4711fc03f3000db2388ec8600a67cb3fadab07a4
TauqeerAhmad5201 <hellotauqeer@gmail.com>
2023-03-02 00:33:21 +0530

    set propTypes and defaultProps in App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import PropTypes from 'prop-types';
 import Navbar from './components/Navbar'
 import Event from './components/Event'
 import Details from './components/Details';
@@ -6,11 +7,23 @@ import Details from './components/Details';
 function App() {
   return (
     <>
-    <Navbar></Navbar>
+    <Navbar Home="Home" Photos="Photos" Vision="Vision" Values="Values"></Navbar>
     <Event></Event>
     <Details></Details>
     </>
   );
 }
 
+Navbar.propTypes = {
+    Home: PropTypes.string.isRequired, 
+    Photos: PropTypes.string.isRequired,
+    Vision: PropTypes.string.isRequired,
+    Values: PropTypes.string.isRequired, 
+}
+Navbar.defaultProps = {
+  Home : "Link 1",
+  Photos : "Link 2", 
+  Vision : "Link 3",
+  Values : "Link 4"
+}
 export default App;
