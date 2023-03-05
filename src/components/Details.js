df4c2a59b1cdb745c331c6384f63ff00146915f7
TauqeerAhmad5201 <hellotauqeer@gmail.com>
2023-03-05 23:49:43 +0530

    added the sub-details of events

--- src/components/Details.js
+++ src/components/Details.js
@@ -11,16 +11,21 @@ export default function Details() {
                 <span className="font-semibold title-font text-gray-700">
                   Innaugration
                 </span>
-                <span className="mt-1 text-gray-500 text-sm">September 2022</span>
+                <span className="mt-1 text-gray-500 text-sm">
+                  September 2022
+                </span>
               </div>
               <div className="md:flex-grow">
                 <p className="leading-relaxed">
-                  Glossier echo park pug, church-key sartorial biodiesel
-                  vexillologist pop-up snackwave ramps cornhole. Marfa 3 wolf
-                  moon party messenger bag selfies, poke vaporware kombucha
-                  lumbersexual pork belly polaroid hoodie portland craft beer.
+                  Innaugrated the Google Developer Student Club in front of
+                  every student and let them know our mission and goal. Our
+                  Mission and goal is to set every student to learn and
+                  implement technology.
                 </p>
-                <a href="https://www.google.com/" className="text-indigo-500 inline-flex items-center mt-4">
+                <a
+                  href="https://www.google.com/"
+                  className="text-indigo-500 inline-flex items-center mt-4"
+                >
                   Learn More
                   <svg
                     className="w-4 h-4 ml-2"
@@ -46,12 +51,14 @@ export default function Details() {
               </div>
               <div className="md:flex-grow">
                 <p className="leading-relaxed">
-                  Glossier echo park pug, church-key sartorial biodiesel
-                  vexillologist pop-up snackwave ramps cornhole. Marfa 3 wolf
-                  moon party messenger bag selfies, poke vaporware kombucha
-                  lumbersexual pork belly polaroid hoodie portland craft beer.
+                  Started our 4 days Android Study Compose Camp. The major aim
+                  of this compose camp was to establish a learning stage of
+                  Android and the students in an university.
                 </p>
-                <a href="https://www.google.com/" className="text-indigo-500 inline-flex items-center mt-4">
+                <a
+                  href="https://www.google.com/"
+                  className="text-indigo-500 inline-flex items-center mt-4"
+                >
                   Learn More
                   <svg
                     className="w-4 h-4 ml-2"
@@ -71,18 +78,21 @@ export default function Details() {
             <div className="py-8 flex flex-wrap md:flex-nowrap">
               <div className="md:w-64 md:mb-0 mb-6 flex-shrink-0 flex flex-col">
                 <span className="font-semibold title-font text-gray-700">
-                  Meta Spark AR 
+                  Meta Spark AR
                 </span>
                 <span className="text-sm text-gray-500">November 2022</span>
               </div>
               <div className="md:flex-grow">
                 <p className="leading-relaxed">
-                  Glossier echo park pug, church-key sartorial biodiesel
-                  vexillologist pop-up snackwave ramps cornhole. Marfa 3 wolf
-                  moon party messenger bag selfies, poke vaporware kombucha
-                  lumbersexual pork belly polaroid hoodie portland craft beer.
+                  Orgranised a wonderful event on Meta Spark AR, which is an
+                  augmented reality technology introduced by Meta(Facebook).
+                  Showcased some cool spark AR filters and made students to dive
+                  into virtual reality and augmented reality.
                 </p>
-                <a href="https://www.google.com/" className="text-indigo-500 inline-flex items-center mt-4">
+                <a
+                  href="https://www.google.com/"
+                  className="text-indigo-500 inline-flex items-center mt-4"
+                >
                   Learn More
                   <svg
                     className="w-4 h-4 ml-2"
@@ -104,16 +114,20 @@ export default function Details() {
                 <span className="font-semibold title-font text-gray-700">
                   DevOps Session
                 </span>
-                <span className="mt-1 text-gray-500 text-sm">February 2023</span>
+                <span className="mt-1 text-gray-500 text-sm">
+                  February 2023
+                </span>
               </div>
               <div className="md:flex-grow">
                 <p className="leading-relaxed">
-                  Glossier echo park pug, church-key sartorial biodiesel
-                  vexillologist pop-up snackwave ramps cornhole. Marfa 3 wolf
-                  moon party messenger bag selfies, poke vaporware kombucha
-                  lumbersexual pork belly polaroid hoodie portland craft beer.
+                  Scratched the basics of Developer Operation in the Jamia
+                  Hamdard University. Invited Kritik Sachdeva, working in IBM
+                  onto the stage to curate and teach things on DevOps.
                 </p>
-                <a href="https://www.google.com/" className="text-indigo-500 inline-flex items-center mt-4">
+                <a
+                  href="https://www.google.com/"
+                  className="text-indigo-500 inline-flex items-center mt-4"
+                >
                   Learn More
                   <svg
                     className="w-4 h-4 ml-2"
